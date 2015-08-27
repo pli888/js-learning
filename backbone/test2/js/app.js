0993f68578f74ab9c859f7c432102964caf8086a
pli888 <peter@gigasciencejournal.com>
2015-08-27 11:11:19 +0800

    Commit files in test2 folder

---
+++ backbone/test2/js/app.js
@@ -0,0 +1,12 @@
+// Used for instantiating app-view.js on loading index.html
+// This is done using jQuery's ready()
+
+var app = app || {};
+var ENTER_KEY = 13;
+
+$(function() {
+
+    // Kick things off by creating the **App**.
+    new app.AppView();
+
+});
